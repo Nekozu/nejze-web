a38cee57d24bda22479ea67b5de29a57111ffb21
nekozu <81870902+nekozu@users.noreply.github.com>
2021-10-21 08:29:00 +0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+  "net/http"
+
+  "github.com/gin-gonic/gin"
+)
+
+var router *gin.Engine
+
+func main() {
+
+  router = gin.Default()
+  router.LoadHTMLGlob("templates/*")
+
+  router.GET("/", func(c *gin.Context) {
+
+    c.HTML(
+      http.StatusOK,
+      "index.html",
+      gin.H{
+        "title": "Home",
+      },
+    )
+
+   })
+  router.GET("/contact", func(c *gin.Context) {
+
+    c.HTML(
+      http.StatusOK,
+      "contact.html",
+      gin.H{
+      },
+    )
+
+  })
+
+  // Start serving the application
+  router.Run()
+
+}
